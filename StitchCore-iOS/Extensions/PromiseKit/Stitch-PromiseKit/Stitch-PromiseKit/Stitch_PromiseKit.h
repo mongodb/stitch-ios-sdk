ae3e51495ff71c49d7c650f307e38485a7acbfed
Jason Flax <jsflax@gmail.com>
2018-04-17 12:44:00 -0400

    Added tests

--- StitchCore-iOS/Extensions/PromiseKit/Stitch-PromiseKit/Stitch-PromiseKit/Stitch_PromiseKit.h
+++ StitchCore-iOS/Extensions/PromiseKit/Stitch-PromiseKit/Stitch-PromiseKit/Stitch_PromiseKit.h
@@ -7,5 +7,3 @@ FOUNDATION_EXPORT double Stitch_PromiseKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char Stitch_PromiseKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Stitch_PromiseKit/PublicHeader.h>
-
-
