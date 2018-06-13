758e542cbf7375ed129bba8df192d18162c77422
Jay Flax <jsflax@gmail.com>
2018-06-13 18:52:33 -0400

    removed garbage

--- iOS/Services/StitchLocalMongoDBService/StitchLocalMongoDBService/StitchLocalMongoDBService.h
+++ iOS/Services/StitchLocalMongoDBService/StitchLocalMongoDBService/StitchLocalMongoDBService.h
@@ -1,11 +1,3 @@
-//
-//  StitchLocalMongoDBService.h
-//  StitchLocalMongoDBService
-//
-//  Created by Jay Flax on 6/13/18.
-//  Copyright © 2018 MongoDB. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for StitchLocalMongoDBService.
