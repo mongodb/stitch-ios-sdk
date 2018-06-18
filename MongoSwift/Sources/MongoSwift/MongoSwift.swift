ca4eba4e69bef2fa7fb3d665b671d47770f072e5
Jay Flax <jsflax@gmail.com>
2018-06-18 09:32:24 -0400

    remove extra fmks

--- MongoSwift/Sources/MongoSwift/MongoSwift.swift
+++
@@ -1,4 +0,0 @@
-/// Returns a `String` indicating the version of the driver.
-public func version() -> String {
-    return "testing"
-}
--- MongoSwift/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-import MongoSwiftTests
-
-var tests = [XCTestCaseEntry]()
-tests += MongoSwiftTests.allTests()
-
-XCTMain(tests)
