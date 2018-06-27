08af10a780b9a626e2ddba05189d5b0205289a82
Jason Flax <jsflax@gmail.com>
2018-06-26 22:16:46 -0400

    download mongoswift

--- scripts/download_mongoswift.sh
+++ scripts/download_mongoswift.sh
@@ -1,7 +1,6 @@
 mkdir -p Sources
 # download MongoSwift
 if [ ! -d Sources/MongoSwift ]; then
-  log_i "downloading MongoSwift"
   curl -# -L https://api.github.com/repos/mongodb/mongo-swift-driver/tarball > mongo-swift.tgz
   mkdir mongo-swift
   # extract mongo-swift
@@ -10,6 +9,4 @@ if [ ! -d Sources/MongoSwift ]; then
   cp -r mongo-swift/Sources/MongoSwift Sources/MongoSwift
   # remove artifacts
   rm -rf mongo-swift mongo-swift.tgz
-else
-  log_w "skipping downloading MongoSwift"
 fi
