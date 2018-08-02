ddebb4ea6813663e0f9e1fd8d2497b196aea5d63
Adam Chelminski <adam.chelminski@mongodb.com>
2018-08-02 14:22:55 -0400

    generate AWS service documentation

--- contrib/generate_docs.sh
+++ contrib/generate_docs.sh
@@ -6,6 +6,7 @@ python scripts/run_xctests.py --test=False
 
 sourcekitten doc --module-name StitchCoreSDK -- -workspace ./Stitch.xcworkspace -scheme StitchCoreSDK-Package -xcconfig DocGen/Configs/kitten.xcconfig > .raw_docs.json
 
+sourcekitten doc --module-name StitchCoreAWSService -- -workspace ./Stitch.xcworkspace -scheme StitchCoreAWSService-Package -xcconfig DocGen/Configs/kitten.coreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchCoreAWSS3Service -- -workspace ./Stitch.xcworkspace -scheme StitchCoreAWSS3Service-Package -xcconfig DocGen/Configs/kitten.coreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchCoreAWSSESService -- -workspace ./Stitch.xcworkspace -scheme StitchCoreAWSSESService-Package -xcconfig DocGen/Configs/kitten.coreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchCoreFCMService -- -workspace ./Stitch.xcworkspace -scheme StitchCoreFCMService-Package -xcconfig DocGen/Configs/kitten.coreservice.xcconfig >> .raw_docs.json
@@ -15,6 +16,7 @@ sourcekitten doc --module-name StitchCoreTwilioService -- -workspace ./Stitch.xc
 
 sourcekitten doc --module-name StitchCore -- -workspace ./Stitch.xcworkspace -scheme StitchCore -sdk macosx -xcconfig DocGen/Configs/kitten.ioscore.xcconfig >> .raw_docs.json
 
+sourcekitten doc --module-name StitchAWSService -- -workspace ./Stitch.xcworkspace -scheme StitchAWSService -sdk macosx -xcconfig DocGen/Configs/kitten.ioscoreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchAWSS3Service -- -workspace ./Stitch.xcworkspace -scheme StitchAWSS3Service -sdk macosx -xcconfig DocGen/Configs/kitten.ioscoreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchAWSSESService -- -workspace ./Stitch.xcworkspace -scheme StitchAWSSESService -sdk macosx -xcconfig DocGen/Configs/kitten.ioscoreservice.xcconfig >> .raw_docs.json
 sourcekitten doc --module-name StitchFCMService -- -workspace ./Stitch.xcworkspace -scheme StitchFCMService -sdk macosx -xcconfig DocGen/Configs/kitten.ioscoreservice.xcconfig >> .raw_docs.json
