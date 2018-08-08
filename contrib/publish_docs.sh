f8945c7d0a2b09c159f8fe4c3c967135108f139e
Adam Chelminski <adam.chelminski@mongodb.com>
2018-08-08 12:03:33 -0400

    fix contrib/publish_docs.sh

--- contrib/publish_docs.sh
+++ contrib/publish_docs.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-VERSION=`cat ../StitchSDK.podspec | grep "spec.version" | head -1 | cut -d \" -f2`
+VERSION=`cat StitchSDK.podspec | grep "spec.version" | head -1 | cut -d \" -f2`
 VERSION_MAJOR=$(echo $VERSION | cut -d. -f1)
 VERSION_MINOR=$(echo $VERSION | cut -d. -f2)
 VERSION_PATCH=$(echo $VERSION | cut -d. -f3 | cut -d- -f1)
-VERSION_QUALIFIER=$(echo $VERSION | cut -d- -f2)
-VERSION_QUALIFIER_INC=$(echo $VERSION | cut -d- -f3)
+VERSION_QUALIFIER=$(echo $VERSION | cut -d- -f2 -s)
+VERSION_QUALIFIER_INC=$(echo $VERSION | cut -d- -f3 -s)
 
-./generate_docs.sh
+contrib/generate_docs.sh
 
 if ! which aws; then
    echo "aws CLI not found. see: https://docs.aws.amazon.com/cli/latest/userguide/installing.html"
@@ -18,12 +18,12 @@ fi
 
 if [ -z "$VERSION_QUALIFIER" ]; then
 	# Publish to MAJOR, MAJOR.MINOR
-	aws s3 cp ../docs s3://stitch-sdks/swift/docs/$VERSION_MAJOR --recursive --acl public-read
-	aws s3 cp ../docs s3://stitch-sdks/swift/docs/$VERSION_MAJOR.$VERSION_MINOR --recursive --acl public-read
+	aws s3 cp docs s3://stitch-sdks/swift/docs/$VERSION_MAJOR --recursive --acl public-read
+	aws s3 cp docs s3://stitch-sdks/swift/docs/$VERSION_MAJOR.$VERSION_MINOR --recursive --acl public-read
 fi
 
 # Publish to full version
-aws s3 cp ../docs s3://stitch-sdks/swift/docs/$VERSION --recursive --acl public-read
+aws s3 cp docs s3://stitch-sdks/swift/docs/$VERSION --recursive --acl public-read
 
 BRANCH_NAME=`git branch | grep -e "^*" | cut -d' ' -f 2`
-aws s3 cp ../docs s3://stitch-sdks/swift/docs/branch/$BRANCH_NAME --recursive --acl public-read
+aws s3 cp docs s3://stitch-sdks/swift/docs/branch/$BRANCH_NAME --recursive --acl public-read
