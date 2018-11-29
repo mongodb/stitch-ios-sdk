5aa79fa195f4e00f20921ea5071f3554f8b5bcc7
Adam Chelminski <adam.chelminski@mongodb.com>
2018-11-29 14:53:37 -0500

    linted darwin twilio service

--- Darwin/Services/StitchTwilioService/StitchTwilioServiceTests/TwilioServiceClientIntTests.swift
+++ Darwin/Services/StitchTwilioService/StitchTwilioServiceTests/TwilioServiceClientIntTests.swift
@@ -1,3 +1,5 @@
+// swiftlint:disable cyclomatic_complexity
+// swiftlint:disable function_body_length
 import XCTest
 import StitchCoreAdminClient
 import StitchCore
@@ -13,17 +15,18 @@ let testTwilioAuthToken = TEST_TWILIO_AUTH_TOKEN.isEmpty ?
 class TwilioServiceClientIntTests: BaseStitchIntTestCocoaTouch {
     override func setUp() {
         super.setUp()
-        
+
         guard !(testTwilioSID?.isEmpty ?? true),
             !(testTwilioAuthToken?.isEmpty ?? true) else {
-            XCTFail("No TWILIO_SID or TWILIO_AUTH_TOKEN preprocessor macros; failing test. See README for more details.")
+            XCTFail("No TWILIO_SID or TWILIO_AUTH_TOKEN preprocessor macros; "
+                    + "failing test. See README for more details.")
             return
         }
     }
-    
+
     func testSendMessage() throws {
         let app = try self.createApp()
-        let _ = try self.addProvider(toApp: app.1, withConfig: ProviderConfigs.anon())
+        _ = try self.addProvider(toApp: app.1, withConfig: ProviderConfigs.anon())
         let svc = try self.addService(
             toApp: app.1,
             withType: "twilio",
@@ -35,23 +38,23 @@ class TwilioServiceClientIntTests: BaseStitchIntTestCocoaTouch {
         _ = try self.addRule(toService: svc.1,
                          withConfig: RuleCreator.actions(name: "rule",
                                                          actions: RuleActionsCreator.twilio(send: true)))
-        
+
         let client = try self.appClient(forApp: app.0)
-        
+
         let exp0 = expectation(description: "should login")
         client.auth.login(withCredential: AnonymousCredential()) { _ in
             exp0.fulfill()
         }
         wait(for: [exp0], timeout: 5.0)
-        
+
         let twilio = client.serviceClient(fromFactory: twilioServiceClientFactory, withName: "twilio1")
-        
+
         // Sending a random message to an invalid number should fail
         let to = "+15005550010"
         let from = "+15005550001"
         let body = "I've got it!"
         let mediaURL = "https://jpegs.com/myjpeg.gif.png"
-        
+
         let exp1 = expectation(description: "should not send message")
         twilio.sendMessage(to: to, from: from, body: body, mediaURL: nil) { result in
             switch result {
@@ -68,7 +71,7 @@ class TwilioServiceClientIntTests: BaseStitchIntTestCocoaTouch {
             exp1.fulfill()
         }
         wait(for: [exp1], timeout: 5.0)
-        
+
         let exp2 = expectation(description: "should not send message")
         twilio.sendMessage(to: to, from: from, body: body, mediaURL: mediaURL) { result in
             switch result {
@@ -85,10 +88,10 @@ class TwilioServiceClientIntTests: BaseStitchIntTestCocoaTouch {
             exp2.fulfill()
         }
         wait(for: [exp2], timeout: 5.0)
-        
+
         // Sending with all good params for Twilio should work
         let fromGood = "+15005550006"
-        
+
         let exp3 = expectation(description: "should send message")
         let exp4 = expectation(description: "should send message")
         twilio.sendMessage(to: to, from: fromGood, body: body, mediaURL: nil) { result in
@@ -110,10 +113,10 @@ class TwilioServiceClientIntTests: BaseStitchIntTestCocoaTouch {
             exp4.fulfill()
         }
         wait(for: [exp3, exp4], timeout: 5.0)
-        
+
         // Excluding any required parameters should fail
         let exp5 = expectation(description: "should have invalid params")
-        
+
         twilio.sendMessage(to: to, from: "", body: body, mediaURL: mediaURL) { result in
             switch result {
             case .success:
